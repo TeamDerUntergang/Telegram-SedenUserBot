728f7c15864c18177d76fda42af1185494a60691
NaytSeyd <NaytSeyd@yandex.com>
2021-01-25 19:00:09 +0300

    seden: afk: Fix Attribute error
    Signed-off-by: NaytSeyd <NaytSeyd@yandex.com>

--- sedenbot/modules/afk.py
+++ sedenbot/modules/afk.py
@@ -26,43 +26,43 @@ TEMP_SETTINGS['COUNT_MSG'] = 0
 
 @sedenify(incoming=True, outgoing=False, disable_edited=True,
           private=False, bot=False, disable_notify=True)
-def mention_afk(mention):
+def mention_afk(msg):
     me = mel[0]
-    if (mention.mentioned or mention.reply_to_message) and (
-            mention.reply_to_message.from_user) and (
-            mention.reply_to_message.from_user.id == me.id):
+    mentioned = msg.mentioned
+    rep_m = msg.reply_to_message
+    if mentioned or rep_m and rep_m.from_user and rep_m.from_user.id == me.id:
         if TEMP_SETTINGS['IS_AFK']:
-            if mention.from_user.id not in TEMP_SETTINGS['AFK_USERS']:
+            if msg.from_user.id not in TEMP_SETTINGS['AFK_USERS']:
                 if 'AFK_REASON' in TEMP_SETTINGS:
                     reply(
-                        mention, get_translation(
+                        msg, get_translation(
                             "afkMessage2", [
                                 '**', me.first_name, me.id,
                                 '`', TEMP_SETTINGS['AFK_REASON']]))
                 else:
-                    reply(mention, f"```{choice(AFKSTR)}```")
-                TEMP_SETTINGS['AFK_USERS'].update({mention.from_user.id: 1})
+                    reply(msg, f"```{choice(AFKSTR)}```")
+                TEMP_SETTINGS['AFK_USERS'].update({msg.from_user.id: 1})
                 TEMP_SETTINGS['COUNT_MSG'] = TEMP_SETTINGS['COUNT_MSG'] + 1
             else:
                 if TEMP_SETTINGS['AFK_USERS'][
-                        mention.from_user.id] % randint(
+                        msg.from_user.id] % randint(
                         2, 4) == 0:
                     if 'AFK_REASON' in TEMP_SETTINGS:
                         reply(
-                            mention, get_translation(
+                            msg, get_translation(
                                 "afkMessage2", [
                                     '**', me.first_name, me.id,
                                     '`', TEMP_SETTINGS['AFK_REASON']]))
                     else:
-                        reply(mention, f"```{choice(AFKSTR)}```")
+                        reply(msg, f"```{choice(AFKSTR)}```")
                     TEMP_SETTINGS['AFK_USERS'][
-                        mention.from_user.id] = TEMP_SETTINGS['AFK_USERS'][
-                        mention.from_user.id] + 1
+                        msg.from_user.id] = TEMP_SETTINGS['AFK_USERS'][
+                        msg.from_user.id] + 1
                     TEMP_SETTINGS['COUNT_MSG'] = TEMP_SETTINGS['COUNT_MSG'] + 1
                 else:
                     TEMP_SETTINGS['AFK_USERS'][
-                        mention.from_user.id] = TEMP_SETTINGS['AFK_USERS'][
-                        mention.from_user.id] + 1
+                        msg.from_user.id] = TEMP_SETTINGS['AFK_USERS'][
+                        msg.from_user.id] + 1
                     TEMP_SETTINGS['COUNT_MSG'] = TEMP_SETTINGS['COUNT_MSG'] + 1
     raise ContinuePropagation
 
