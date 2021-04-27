dd6e307d86403d808b4eed6fcb6680d33271a8ba
Mustafa Çalışkan <musfay@protonmail.com>
2021-04-27 19:08:55 +0300

    launch bot if config.env exists in shell.nix

--- shell.nix
+++ shell.nix
@@ -12,8 +12,13 @@ stdenv.mkDerivation {
     export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
     export PATH="$PIP_PREFIX/bin:$PATH"
     export PIP="${pkgs.python3.pkgs.pip.outPath}/bin/pip"
+    export PYTHON="${pkgs.python3.outPath}/bin/python"
     unset SOURCE_DATE_EPOCH
     
     $PIP install -r requirements.txt
+    
+    if [ -f "config.env" ]; then
+      $PYTHON seden.py
+    fi
     '';
 }
