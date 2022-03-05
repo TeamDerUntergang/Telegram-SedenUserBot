3cff9109abdc63a3a49749a24eddf9aa28d9ebf0
fatihesergg <fatihesergg@gmail.com>
2022-03-05 21:56:07 +0300

    Release Seden v1.5.4
    Signed-off-by: fatihesergg <fatihesergg@gmail.com>

--- sedenbot/__init__.py
+++ sedenbot/__init__.py
@@ -118,7 +118,7 @@ if not API_HASH:
     LOGS.warn(get_translation('apiHashError'))
     quit(1)
 
-BOT_VERSION = '1.5.3'
+BOT_VERSION = '1.5.4'
 SUPPORT_GROUP = 'SedenUserBotSupport'
 CHANNEL = 'SedenUserBot'
 
