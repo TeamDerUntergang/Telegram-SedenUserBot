c742d2d8303797f5bb088504e8a86a59526cb9a8
NaytSeyd <naytseyd@yandex.com>
2022-03-16 23:55:10 +0300

    Release Seden v1.5.6
    Signed-off-by: NaytSeyd <naytseyd@yandex.com>

--- sedenbot/__init__.py
+++ sedenbot/__init__.py
@@ -117,7 +117,7 @@ if not API_HASH:
     LOGS.warn(get_translation('apiHashError'))
     quit(1)
 
-BOT_VERSION = '1.5.5'
+BOT_VERSION = '1.5.6'
 SUPPORT_GROUP = 'SedenUserBotSupport'
 CHANNEL = 'SedenUserBot'
 
