68d63b7a53d79a3b7b9ba863e9047cc6b343b6a8
squirrelpython <dphenix4570@gmail.com>
2022-03-16 15:03:59 +0300

    Cargo tracking module has been updated, other companies have been added.

--- sedenbot/modules/kargotakip.py
+++ sedenbot/modules/kargotakip.py
@@ -6,76 +6,96 @@
 #
 # All rights reserved. See COPYING, AUTHORS.
 #
-
-from ast import arg
-from requests import get, post
+from requests import get
 from json import JSONDecodeError
 from sedenbot import HELP, SEDEN_LANG
 from sedenecem.core import (
     edit,
-    extract_args,
     get_translation,
     sedenify,
 )
 
 
+def parseShipEntity(jsonEntity: dict) -> str:
+    if SEDEN_LANG == 'en':
+        text = f"""
+<strong>Company:</strong> <code>{jsonEntity['data']['company'].title()}</code>
+<strong>Track ID:</strong> <code>{jsonEntity['data']['tracking_no']}</code>
+<strong>Status:</strong> <code>{jsonEntity['data']['status']}</code>
+<strong>Sender:</strong> <code>{jsonEntity['data']['sender_name']}</code>
+<strong>Receiver:</strong> <code>{jsonEntity['data']['receiver_name']}</code>
+<strong>Sender Unit:</strong> <code>{jsonEntity['data']['sender_unit']}</code>
+<strong>Receiver Unit:</strong> <code>{jsonEntity['data']['receiver_unit']}</code>
+<strong>Sended Date:</strong> <code>{jsonEntity['data']['sended_date']}</code>
+<strong>Delivered Date:</strong> <code>{jsonEntity['data']['delivered_date']}</code>
+
+<strong><u>Last movement</u></strong>
+
+"""
+    movements = ""
+    for movement in jsonEntity['data']['movements'][-1:]:
+        movements += f"<code>Unit: {movement['unit']}\nStatus: {movement['status']}\nDate: {movement['date']}\nTime: {movement['time']}\nAction: {movement['action']}</code>\n\n"
+    
+    if SEDEN_LANG == 'tr':
+        text = f"""
+<strong>Firma:</strong> <code>{jsonEntity['data']['company'].title()}</code>
+<strong>Takip No:</strong> <code>{jsonEntity['data']['tracking_no']}</code>
+<strong>Durum:</strong> <code>{jsonEntity['data']['status']}</code>
+<strong>Gönderici:</strong> <code>{jsonEntity['data']['sender_name']}</code>
+<strong>Alıcı:</strong> <code>{jsonEntity['data']['receiver_name']}</code>
+<strong>Gönderim yeri:</strong> <code>{jsonEntity['data']['sender_unit']}</code>
+<strong>Alım yeri:</strong> <code>{jsonEntity['data']['receiver_unit']}</code>
+<strong>Gönderi tarihi:</strong> <code>{jsonEntity['data']['sended_date']}</code>
+<strong>Teslim tarihi:</strong> <code>{jsonEntity['data']['delivered_date']}</code>
+
+<strong><u>Son hareket</u></strong>
+
+"""
+    movements = ""
+    for movement in jsonEntity['data']['movements'][-1:]:
+        movements += f"<code>Yer: {movement['unit']}\nDurum: {movement['status']}\nTarih: {movement['date']}\nZaman: {movement['time']}\nİşlem: {movement['action']}</code>\n\n"
+    
+    text += movements
+    return text
 
 
-def getSession():
-    headers = {
-        'accept': 'application/json',
-        'accept-encoding': 'gzip',
-        'accept-language': 'tr',
-        'host': 'customerappapi.yurticikargo.com',
-        'user-agent': 'okhttp/4.4.0',
-        'yk-app-token': 'Android407',
-        'yk-mobile-device-id': 'd2926855-ed39-4f05-afcf-a9f350829427',
-        'yk-mobile-os': 'Android',}
-
-    response = post('https://customerappapi.yurticikargo.com/KOPSRestServices/rest/customermobile/auth/session', headers=headers)
-    return response.json()['sessionToken']
-
-def getKargoEntity(kargoId):
+def getShipEntity(company: str, trackId: int or str) -> dict or None:
+    headers: dict = {
+        'platform': 'Android',
+        'public': 'lfJGmU9XpGZcMwyLtZBk',
+        'secret': 'sMPMnQuc51nmcBbaeOK1',
+        'unique': 'afb612018716663e',
+    }
+    response = get(f"https://tapi.kolibu.com/{company}/{trackId}", headers=headers)
     try:
-        headers = {
-        'accept': 'application/json',
-        'accept-encoding': 'gzip',
-        'authorization': getSession(),
-        'user-agent': 'okhttp/4.4.0',
-        'yk-app-token': 'Android407',
-        'yk-mobile-device-id': 'd2926855-ed39-4f05-afcf-a9f350829427',
-        'yk-mobile-os': 'Android',
-        'accept-language': 'tr',
-        }
-        response = get(f'https://customerappapi.yurticikargo.com/KOPSRestServices/rest/customermobile/shipments/tracking/{kargoId}/detail', headers=headers).json()
-        return response
+        return response.json() if response.json()['success'] else None
     except JSONDecodeError:
         return None
 
-
-@sedenify(pattern='^.yurtici')
-def yurticiKargo(message):
+@sedenify(pattern='^.(yurtici|aras|ptt)')
+def shippingTrack(message):
     edit(message, f"`{get_translation('processing')}`")
-    argv = extract_args(message)
-    takipNo = argv.split(' ', 1)[0]
-    if not takipNo or len(takipNo) != 12:
+    argv = message.text.split(' ')
+    if len(argv) > 2:
         edit(message, f"`{get_translation('wrongCommand')}`")
         return
-    dateParse = lambda raw_date: "{}.{}.{}".format(*[raw_date[:4], raw_date[4:6], raw_date[6:8]])
-    kargo_data = getKargoEntity(takipNo)
-    text = f"""
-<strong>Firma:</strong> <code>Yurtici Kargo</code>
-<strong>Durum:</strong> <code>{kargo_data['shipmentStatus'].title()}</code>
-<strong>Gönderen:</strong> <code>{kargo_data['sender'].replace("******************", "")}</code>
-<strong>Alıcı:</strong> <code>{kargo_data['receiver'].replace("******************", "")}</code>
-<strong>Tahmini Teslim:</strong> <code>{dateParse(kargo_data['estimatedDeliveryDate'])}</code>
-<strong>Çıkış Tarihi:</strong> <code>{dateParse(kargo_data['shipmentDate'])}</code>
-<strong>Gönderi Kodu:</strong> <code>{kargo_data['id']}</code>
-<strong>Teslim Birimi:</strong> <code>{kargo_data['deliveryUnitName']}</code>
-"""
-    edit(message, text, parse='HTML')
+    comp, trackId = argv
+    if not trackId:
+        edit(message, f"`{get_translation('wrongCommand')}`")
+        return
+    if comp == '.yurtici':
+        kargo_data = getShipEntity(company="yurtici", trackId=trackId)
+    if comp == '.aras':
+        kargo_data = getShipEntity(company="aras", trackId=trackId)
+    if comp == '.ptt':
+        kargo_data = getShipEntity(company="ptt", trackId=trackId)
+    if kargo_data:
+        text = parseShipEntity(kargo_data)
+        edit(message, text, parse='HTML')
+        return
+    edit(message, '`Tracking information not found!`' if SEDEN_LANG == 'en' else '`Takip bilgisi bulunamadı!`')
 
 
 
 
-HELP.update({'kargotakip': get_translation("shippingTrack")})
+HELP.update({'shippingtrack': get_translation("shippingTrack")})
