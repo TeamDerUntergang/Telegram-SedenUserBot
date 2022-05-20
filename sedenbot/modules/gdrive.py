cc4af73613d8c0d9336e665e65d28fcdde6efad0
fatihesergg <fatihesergg@gmail.com>
2022-05-20 19:11:40 +0300

    gdrive: Add license

--- sedenbot/modules/gdrive.py
+++ sedenbot/modules/gdrive.py
@@ -1,3 +1,12 @@
+# Copyright (C) 2020-2022 TeamDerUntergang <https://github.com/TeamDerUntergang>
+#
+# This file is part of TeamDerUntergang project,
+# and licensed under GNU Affero General Public License v3.
+# See the GNU Affero General Public License for more details.
+#
+# All rights reserved. See COPYING, AUTHORS.
+#
+
 import pickle
 from os import path, remove, replace
 from re import match, search
