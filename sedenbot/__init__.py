90f103cdbbaa84d85b844847ef34126e30f2ef31
NaytSeyd <naytseyd@yandex.com>
2023-08-01 00:20:12 +0300

    Release Seden v1.7.5

--- sedenbot/__init__.py
+++ sedenbot/__init__.py
@@ -114,7 +114,7 @@ if not API_HASH:
     LOGS.warn(get_translation('apiHashError'))
     quit(1)
 
-BOT_VERSION = '1.7.4'
+BOT_VERSION = '1.7.5'
 SUPPORT_GROUP = 'SedenUserBotSupport'
 CHANNEL = 'SedenUserBot'
 
